8bca6ddb0d7e3ca2bc2f61db182507c100dc9797
Myrel666 <egaalmirajp@gmail.com>
2023-03-20 22:31:16 +0700

    Version 1

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import ProductDisplay from './components/ProductDisplay.vue';
+
+export default{
+  components: {
+    ProductDisplay
+  }
+}
+</script>
+
+<template lang="">
+<div id="app">
+  <ProductDisplay/>
+</div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+<div :style="Background" id="background">
+</div>
+<div :style="box" id="box">
+</div>
+<button :style="button-buynow" id="button-buynow">
+    Buy Now
+</button>
+<button :style="button-nextnow" id="button-nextproduct">
+    Next Product
+</button>
+
+</template>
+<script>
+
+</script>
